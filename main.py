4f1051fe9009b2d0bdd4838040a6e0206f2c5c64
bear <bear@weedo.hk>
2019-04-22 11:31:41 +0800

    update

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ from project.base.config import ReportFlag
 if __name__ == '__main__':
     flags = [
         ReportFlag.mail,
-        ReportFlag.lost_level,
-        ReportFlag.retention_level,
-        ReportFlag.stage,
-        ReportFlag.new_ads,
-        ReportFlag.retention_ads,
-        ReportFlag.total_ads,
-        ReportFlag.iap_behaviour,
-        ReportFlag.lost_behaviour,
-        ReportFlag.retention_behaviour
+        # ReportFlag.lost_level,
+        # ReportFlag.retention_level,
+        # ReportFlag.stage,
+        # ReportFlag.new_ads,
+        # ReportFlag.retention_ads,
+        # ReportFlag.total_ads,
+        # ReportFlag.iap_behaviour,
+        # ReportFlag.lost_behaviour,
+        # ReportFlag.retention_behaviour
     ]
     option = ReportFlag(flags).option
 
