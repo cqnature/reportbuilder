349005a15df5c837293fab0d0bf4d36f5cc60930
bear <bear@weedo.hk>
2019-05-01 00:31:41 +0800

    update

--- project/entry.py
+++ project/entry.py
@@ -8,4 +8,4 @@ from idlesheep.entry import Entry as IdleSheepEntry
 def generate_report(option, *parameter):
     MergeFoodEntry(option, *parameter).generate_report()
     # MergeGardenEntry(option, *parameter).generate_report()
-    # IdleSheepEntry(option, *parameter).generate_report()
+    IdleSheepEntry(option, *parameter).generate_report()
