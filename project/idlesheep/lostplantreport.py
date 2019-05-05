140d658dc9b3e4c6fa28f9eebf6c1da29cd34093
bear <bear@weedo.hk>
2019-05-05 09:50:26 +0800

    update

--- project/idlesheep/lostplantreport.py
+++ project/idlesheep/lostplantreport.py
@@ -29,7 +29,7 @@ class Report(BaseReport):
         return [self.output_filepath]
 
     def get_plane_max_level(self):
-        return 4;
+        return 5;
 
     def generate_lostplant_report_at_date(self, report_lines, date):
         print("generate_lostplant_report_at_date ", date)
