a46c72e760111f98ae547464231dc5c8de120e0f
bear <bear@weedo.hk>
2019-05-08 00:26:38 +0800

    update

--- project/idlesheep/stagereport.py
+++ project/idlesheep/stagereport.py
@@ -28,14 +28,6 @@ class Report(BaseReport):
             out.close()
         return [self.output_filepath]
 
-    def get_event_configs(self):
-        event_configs = None
-        with open(os.path.join(self.project_config.etc_path, "event.json")) as file:
-            file_config = json.load(file)
-            event_configs = file_config['events']
-            file.close()
-        return event_configs
-
     def generate_stage_report_at_date(self, report_lines, date):
         print("generate_stage_report_at_date ", date)
         with open(self.etc_filepath) as file:
@@ -45,19 +37,21 @@ class Report(BaseReport):
 
             lineIndex = 0
             lines = file.readlines()
-            signup_day_progress_lines = [x.strip() for x in lines[0:4]]
+            signup_day_progress_lines = [x.strip() for x in lines[0:3]]
             signup_day_progress_results = self.get_result("stage_progress_of_signup_users.sql", date)
-            signup_day_progress_lines[1] = signup_day_progress_lines[1].format(Date(date).formatmd())
-            signup_day_progress_lines[3] = signup_day_progress_lines[3].format(firstopen_usercount, 100)
+            signup_day_progress_lines[0] = signup_day_progress_lines[0].format(Date(date).formatmd())
+            signup_day_progress_lines[2] = signup_day_progress_lines[2].format(firstopen_usercount, 100)
             signup_base_datas = []
             for row in signup_day_progress_results:
                 signup_base_data = [row.rebirth, row.level, row.user_count, 100*float(row.user_count)/float(firstopen_usercount)]
                 signup_base_datas.append(signup_base_data)
+            first_level_usercount = firstopen_usercount - sum(t[2] for t in signup_base_datas)
+            signup_base_datas.insert(0, [0, 1, first_level_usercount, 100*float(first_level_usercount)/float(firstopen_usercount)])
             for k in range(len(signup_base_datas)):
                 data = signup_base_datas[k]
                 signup_day_progress_lines.append("{0}-{1},{2},{3:.2f}%,".format(data[0], data[1], data[2], data[3]))
             for k in range(len(signup_day_progress_lines)):
-                append_line(report_lines, lineIndex + k, signup_day_progress_lines[k], k != 0)
+                append_line(report_lines, lineIndex + k, signup_day_progress_lines[k])
             lineIndex += len(signup_day_progress_lines)
 
             lost_base_datas = []
@@ -68,18 +62,25 @@ class Report(BaseReport):
             current_lost_usercount = self.get_lost_count(date, single_date)
             # 流失分布查询
             lost_day_results = self.get_result("stage_progress_of_lost_users.sql", date, single_date)
-            lost_day_progress_lines.extend([x.strip() for x in lines[4:10]])
+            lost_day_progress_lines.extend([x.strip() for x in lines[3:8]])
             for row in lost_day_results:
                 lost_base_data = [row.rebirth, row.level, row.user_count, 100*float(row.user_count)/float(firstopen_usercount)]
+                lost_base_datas.append(lost_base_data)
+            first_lost_usercount = current_lost_usercount - sum(t[2] for t in lost_base_datas)
+            lost_base_datas.insert(0, [0, 1, first_lost_usercount, 100*float(first_lost_usercount)/float(firstopen_usercount)])
+
             lost_base_usercount = current_lost_usercount
-            lost_day_progress_lines[1] = lost_day_progress_lines[1].format(Date(date).formatmd())
-            lost_day_progress_lines[3] = lost_day_progress_lines[3].format(firstopen_usercount, 100)
-            lost_day_progress_lines[4] = lost_day_progress_lines[4].format(firstopen_usercount - current_lost_usercount, 100*float(firstopen_usercount - current_lost_usercount)/float(firstopen_usercount))
-            lost_day_progress_lines[5] = lost_day_progress_lines[5].format(lost_base_usercount, 100* float(lost_base_usercount)/float(firstopen_usercount))
+            lost_day_progress_lines[0] = lost_day_progress_lines[0].format(Date(date).formatmd())
+            lost_day_progress_lines[2] = lost_day_progress_lines[2].format(firstopen_usercount, 100)
+            lost_day_progress_lines[3] = lost_day_progress_lines[3].format(firstopen_usercount - current_lost_usercount, 100*float(firstopen_usercount - current_lost_usercount)/float(firstopen_usercount))
+            lost_day_progress_lines[4] = lost_day_progress_lines[4].format(lost_base_usercount, 100* float(lost_base_usercount)/float(firstopen_usercount))
             for k in range(len(lost_base_datas)):
                 data = lost_base_datas[k]
                 lost_day_progress_lines.append("{0}-{1},{2},{3:.2f}%,".format(data[0], data[1], data[2], data[3]))
             # 数据拼接
             for k in range(len(lost_day_progress_lines)):
-                append_line(report_lines, lineIndex + k, lost_day_progress_lines[k], k != 0)
+                append_line(report_lines, lineIndex + k, lost_day_progress_lines[k])
+            lineIndex += len(lost_day_progress_lines)
+            for k in range(lineIndex, 400):
+                append_line(report_lines, k, ",,,")
             file.close()
