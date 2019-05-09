4288ad77799d82af2081f0470137c963d8040482
bear <bear@weedo.hk>
2019-05-09 11:49:17 +0800

    update

--- project/idlesheep/stagereport.py
+++ project/idlesheep/stagereport.py
@@ -59,28 +59,30 @@ class Report(BaseReport):
             lost_day_progress_lines = []
             # 留存率查询
             single_date = Date(date).adddays(1)
-            current_lost_usercount = self.get_lost_count(date, single_date)
-            # 流失分布查询
-            lost_day_results = self.get_result("stage_progress_of_lost_users.sql", date, single_date)
-            lost_day_progress_lines.extend([x.strip() for x in lines[3:8]])
-            for row in lost_day_results:
-                lost_base_data = [row.rebirth, row.level, row.user_count, 100*float(row.user_count)/float(firstopen_usercount)]
-                lost_base_datas.append(lost_base_data)
-            first_lost_usercount = current_lost_usercount - sum(t[2] for t in lost_base_datas)
-            lost_base_datas.insert(0, [0, 1, first_lost_usercount, 100*float(first_lost_usercount)/float(firstopen_usercount)])
+            if Date(single_date).between(self.end_date) > 0:
+                current_lost_usercount = self.get_lost_count(date, single_date)
+                # 流失分布查询
+                lost_day_results = self.get_result("stage_progress_of_lost_users.sql", date, single_date)
+                lost_day_progress_lines.extend([x.strip() for x in lines[3:8]])
+                for row in lost_day_results:
+                    lost_base_data = [row.rebirth, row.level, row.user_count, 100*float(row.user_count)/float(firstopen_usercount)]
+                    lost_base_datas.append(lost_base_data)
+                first_lost_usercount = current_lost_usercount - sum(t[2] for t in lost_base_datas)
+                lost_base_datas.insert(0, [0, 1, first_lost_usercount, 100*float(first_lost_usercount)/float(firstopen_usercount)])
 
-            lost_base_usercount = current_lost_usercount
-            lost_day_progress_lines[0] = lost_day_progress_lines[0].format(Date(date).formatmd())
-            lost_day_progress_lines[2] = lost_day_progress_lines[2].format(firstopen_usercount, 100)
-            lost_day_progress_lines[3] = lost_day_progress_lines[3].format(firstopen_usercount - current_lost_usercount, 100*float(firstopen_usercount - current_lost_usercount)/float(firstopen_usercount))
-            lost_day_progress_lines[4] = lost_day_progress_lines[4].format(lost_base_usercount, 100* float(lost_base_usercount)/float(firstopen_usercount))
-            for k in range(len(lost_base_datas)):
-                data = lost_base_datas[k]
-                lost_day_progress_lines.append("{0}-{1},{2},{3:.2f}%,".format(data[0], data[1], data[2], data[3]))
-            # 数据拼接
-            for k in range(len(lost_day_progress_lines)):
-                append_line(report_lines, lineIndex + k, lost_day_progress_lines[k])
-            lineIndex += len(lost_day_progress_lines)
+                lost_base_usercount = current_lost_usercount
+                lost_day_progress_lines[0] = lost_day_progress_lines[0].format(Date(date).formatmd())
+                lost_day_progress_lines[2] = lost_day_progress_lines[2].format(firstopen_usercount, 100)
+                lost_day_progress_lines[3] = lost_day_progress_lines[3].format(firstopen_usercount - current_lost_usercount, 100*float(firstopen_usercount - current_lost_usercount)/float(firstopen_usercount))
+                lost_day_progress_lines[4] = lost_day_progress_lines[4].format(lost_base_usercount, 100* float(lost_base_usercount)/float(firstopen_usercount))
+                for k in range(len(lost_base_datas)):
+                    data = lost_base_datas[k]
+                    lost_day_progress_lines.append("{0}-{1},{2},{3:.2f}%,".format(data[0], data[1], data[2], data[3]))
+                # 数据拼接
+                for k in range(len(lost_day_progress_lines)):
+                    append_line(report_lines, lineIndex + k, lost_day_progress_lines[k])
+                lineIndex += len(lost_day_progress_lines)
+                
             for k in range(lineIndex, 400):
                 append_line(report_lines, k, ",,,")
             file.close()
