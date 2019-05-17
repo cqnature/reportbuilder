66cc21ac87702fcb79dade85676462a00a78763d
bear <bear@weedo.hk>
2019-05-17 12:32:31 +0800

    add new idlesheepcn

--- project/entry.py
+++ project/entry.py
@@ -6,10 +6,12 @@ from mergegarden.entry import Entry as MergeGardenEntry
 from idlesheep.entry import Entry as IdleSheepEntry
 from mergegardenjrtt.entry import Entry as MergeGardenJrttEntry
 from idlesheepjrtt.entry import Entry as IdleSheepJrttEntry
+from idlesheepcn.entry import Entry as IdleSheepCnEntry
 
 def generate_report(option, *parameter):
-    MergeFoodEntry(option, *parameter).generate_report()
+    # MergeFoodEntry(option, *parameter).generate_report()
     # MergeGardenEntry(option, *parameter).generate_report()
     # MergeGardenJrttEntry(option, *parameter).generate_report()
-    IdleSheepEntry(option, *parameter).generate_report()
+    # IdleSheepEntry(option, *parameter).generate_report()
+    IdleSheepCnEntry(option, *parameter).generate_report()
     # IdleSheepJrttEntry(option, *parameter).generate_report()
