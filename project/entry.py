d0e205a8a09273e4f41d61a6188f6397e7f96b41
bear <bear@weedo.hk>
2019-06-30 20:16:41 +0800

    update

--- project/entry.py
+++ project/entry.py
@@ -10,10 +10,10 @@ from idlesheepcn.entry import Entry as IdleSheepCnEntry
 from idledisaster.entry import Entry as IdleDisasterEntry
 
 def generate_report(option, *parameter):
-    MergeFoodEntry(option, *parameter).generate_report()
+    # MergeFoodEntry(option, *parameter).generate_report()
     # MergeGardenEntry(option, *parameter).generate_report()
     # MergeGardenJrttEntry(option, *parameter).generate_report()
-    IdleSheepEntry(option, *parameter).generate_report()
-    IdleSheepCnEntry(option, *parameter).generate_report()
+    # IdleSheepEntry(option, *parameter).generate_report()
+    # IdleSheepCnEntry(option, *parameter).generate_report()
     # IdleSheepJrttEntry(option, *parameter).generate_report()
     IdleDisasterEntry(option, *parameter).generate_report()
