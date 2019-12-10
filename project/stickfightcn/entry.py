09cf51955f4531b08b8c9907e9d0c4d8797a8731
bear <bear@weedo.hk>
2019-12-10 14:21:21 +0800

    update

--- project/stickfightcn/entry.py
+++ project/stickfightcn/entry.py
@@ -37,8 +37,8 @@ class Entry(BaseEntry):
         print 'idlesheep do_generate_report'
         report_filepaths = []
         # 开启代理
-        if self.option & ReportFlag.mail:
-            report_filepaths.extend(generate_mail_report(self.query_config, self.start_date))
+        # if self.option & ReportFlag.mail:
+        #     report_filepaths.extend(generate_mail_report(self.query_config, self.start_date))
         if self.option & ReportFlag.lost_level:
             report_filepaths.extend(generate_lostplant_report(self.query_config, self.start_date))
         # if self.option & ReportFlag.retention_level:
