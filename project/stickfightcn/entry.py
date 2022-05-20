d7925cdd3e024ad3ce938eb2054611265c170499
bear <bear@peakxgames.com>
2022-05-20 11:12:39 +0800

    update

--- project/stickfightcn/entry.py
+++ project/stickfightcn/entry.py
@@ -6,26 +6,25 @@ import json
 from project.base.configs import *
 from project.base.date import *
 from project.base.entry import *
-from mailreport import generate_mail_report
-from lostplantreport import generate_lostplant_report
-from retentionplantreport import generate_retentionplant_report
-from retentionadsreport import generate_retention_ads_report
-from newadsreport import generate_new_ads_report
-from totaladsreport import generate_total_ads_report
-from stagereport import generate_stage_report
-from retentionstagereport import generate_retention_stage_report
-from daystagereport import generate_day_stage_report
-from buttonbehaviourreport import generate_button_behaviour_report
-from lostbehaviourreport import generate_lostbehaviour_report
-from lostadsreport import generate_lost_ads_report
-from retentionadscountreport import generate_retention_ads_count_report
+from project.stickfightcn.mailreport import generate_mail_report
+from project.stickfightcn.lostplantreport import generate_lostplant_report
+from project.stickfightcn.retentionplantreport import generate_retentionplant_report
+from project.stickfightcn.retentionadsreport import generate_retention_ads_report
+from project.stickfightcn.newadsreport import generate_new_ads_report
+from project.stickfightcn.totaladsreport import generate_total_ads_report
+from project.stickfightcn.stagereport import generate_stage_report
+from project.stickfightcn.retentionstagereport import generate_retention_stage_report
+from project.stickfightcn.daystagereport import generate_day_stage_report
+from project.stickfightcn.buttonbehaviourreport import generate_button_behaviour_report
+from project.stickfightcn.lostbehaviourreport import generate_lostbehaviour_report
+from project.stickfightcn.lostadsreport import generate_lost_ads_report
+from project.stickfightcn.retentionadscountreport import generate_retention_ads_count_report
 
 
 class Entry(BaseEntry):
     def __init__(self, option, *parameter):
         super(Entry, self).__init__(option, *parameter)
-        self.detail_email.extend(['eli@peakxgames.com', 'kinder@peakxgames.com',
-                                 'near@peakxgames.com', 'nero@peakxgames.com', 'bear@peakxgames.com'])
+        self.detail_email.extend(['bear@peakxgames.com'])
 
     def generate_report(self):
         package_name = __name__.split('.')[-2]
@@ -36,19 +35,20 @@ class Entry(BaseEntry):
         return super(Entry, self).generate_report()
 
     def do_generate_report(self):
-        print('idlesheep do_generate_report')
+        print('stickfightcn do_generate_report')
         report_filepaths = []
         # 开启代理
-        # if self.option & ReportFlag.mail:
-        #     report_filepaths.extend(generate_mail_report(self.query_config, self.start_date))
-        if self.option & ReportFlag.lost_level:
-            report_filepaths.extend(generate_lostplant_report(
+        if self.option & ReportFlag.mail:
+            report_filepaths.extend(generate_mail_report(
                 self.query_config, self.start_date))
+        # if self.option & ReportFlag.lost_level:
+        #     report_filepaths.extend(generate_lostplant_report(
+        #         self.query_config, self.start_date))
         # if self.option & ReportFlag.retention_level:
         #     report_filepaths.extend(generate_retentionplant_report(self.query_config, self.start_date))
-        if self.option & ReportFlag.lost_stage:
-            report_filepaths.extend(generate_stage_report(
-                self.query_config, self.start_date))
+        # if self.option & ReportFlag.lost_stage:
+        #     report_filepaths.extend(generate_stage_report(
+        #         self.query_config, self.start_date))
         # if self.option & ReportFlag.retention_stage:
         #     report_filepaths.extend(generate_retention_stage_report(self.query_config, self.start_date))
         # if self.option & ReportFlag.new_ads:
