c374cc381ff72709c6dfd53e7e56518623c9d937
bear <bear@peakxgames.com>
2022-06-19 04:11:00 +0800

    update

--- project/idlegongfu/lostpushreport.py
+++ project/idlegongfu/lostpushreport.py
@@ -3,15 +3,14 @@
 
 import os
 import json
+import sys
 from project.base.date import *
 from project.base.helper import *
 from project.base.query import *
 from project.base.report import *
 
-extra_retation_date = [18, 21, 24, 27, 30]
-max_lately_date = 15
-max_chapter_id = 15
-lost_day = 1
+lost_day = [2, 3, 4, 5, 6]
+extra_lost_day = [7]
 
 
 def generate_lost_push_report(query_config, date):
@@ -21,10 +20,10 @@ def generate_lost_push_report(query_config, date):
 class Report(BaseReport):
     def __init__(self, query_config, date):
         super(Report, self).__init__(query_config, date)
-        self.etc_filename = 'plant_progress_of_lost_users.csv'
+        self.etc_filename = '流失玩家推进占比.csv'
         country_string = self.query_config.geo_country
         platform_string = self.query_config.platform
-        self.output_filename = "{0}-{1}-LostUser-Chapter-{2}.csv".format(
+        self.output_filename = "{0}-{1}-LostUser-Stage-{2}.csv".format(
             country_string, platform_string, self.end_date)
 
     def do_generate(self):
@@ -34,28 +33,26 @@ class Report(BaseReport):
             with open(self.etc_filepath) as file:
                 lines = file.readlines()
                 head_lines1 = [x.strip() for x in lines[0:2]]
+                # 日期和新增
                 for k in range(len(head_lines1)):
                     append_line(report_lines, k, head_lines1[k])
-                head_lines2 = [x.strip() for x in lines[2:4]]
-                for k in range(len(head_lines2)):
-                    append_line(report_lines, k, head_lines2[k])
-                head_lines3 = [x.strip() for x in lines[4:6]]
-                for k in range(len(head_lines3)):
-                    append_line(report_lines, k, head_lines3[k])
-                # head_lines4 = [x.strip() for x in lines[6:8]]
-                # for d in range(13):
-                #     for k in range(len(head_lines4)):
-                #         append_line(report_lines, k, head_lines4[k].format(d + 3))
-                # for d in extra_retation_date:
-                #     for k in range(len(head_lines4)):
-                #         append_line(report_lines, k, head_lines4[k].format(d))
+                # 2-6天
+                for d in lost_day:
+                    startline = 2 + 3 * (d - 2)
+                    endline = 4 + 3 * (d - 2)
+                    head_lines2 = [x.strip() for x in lines[startline:endline]]
+                    for k in range(len(head_lines2)):
+                        append_line(report_lines, k, head_lines2[k])
+                # 7天
+                head_lines3 = [x.strip() for x in lines[17:19]]
+                for d in extra_lost_day:
+                    append_line(report_lines, 0, head_lines3[0].format(d))
+                    append_line(report_lines, 1, head_lines3[1])
                 file.close()
             for single_date in self.extra_date:
                 self.generate_lost_push_report_at_date(
                     report_lines, single_date)
-            lately_date = max(Date(self.end_date).adddays(
-                1 - max_lately_date), self.start_date)
-            for single_date in Date(lately_date).rangeto(self.end_date, True):
+            for single_date in Date(self.start_date).rangeto(self.end_date, True):
                 self.generate_lost_push_report_at_date(
                     report_lines, single_date)
             reportstring = '\n'.join(report_lines)
@@ -63,118 +60,74 @@ class Report(BaseReport):
             out.close()
         return [self.output_filepath]
 
-    def get_plane_chapter_id(self):
-        return max_chapter_id
-
     def generate_lost_push_report_at_date(self, report_lines, date):
         print("generate_lost_push_report_at_date ", date)
-        chapter_id = self.get_plane_chapter_id()
         with open(self.etc_filepath) as file:
             firstopen_usercount = self.get_firstopen_count(date)
             if firstopen_usercount == 0:
                 return
 
+            lines = file.readlines()
             line_string = ""
             line_string += "{0},".format(Date(date).formatmd())
-            signup_day_progress_results = self.get_result(
-                "plant_progress_of_signup_users.sql", date)
-            total_level_user_count = 0
-            signup_base_datas = []
-            progress_data_map = {}
-            for k in range(0, chapter_id):
-                signup_base_data = [k + 1, 0, 0]
-                signup_base_datas.append(signup_base_data)
-                progress_data_map[k + 1] = signup_base_data
-            for row in signup_day_progress_results:
-                progress_data = progress_data_map[row.chapter_id]
-                progress_data[1] = row.user_count
-                progress_data[2] = 100 * \
-                    float(row.user_count)/float(firstopen_usercount)
-                total_level_user_count += row.user_count
-            first_progress_data = progress_data_map[1]
-            first_progress_data[1] = first_progress_data[1] + \
-                firstopen_usercount - total_level_user_count
-            first_progress_data[2] = 100 * \
-                float(first_progress_data[1])/float(firstopen_usercount)
             line_string += "{0},".format(firstopen_usercount)
-            for k in range(len(signup_base_datas)):
-                data = signup_base_datas[k]
-                line_string += "{0:.2f}%,".format(data[2])
+            all_lost_day = []
+            all_lost_day.extend(lost_day)
+            all_lost_day.extend(extra_lost_day)
 
-            currentDayIndex = 1
-            lost_base_datas = []
-            lost_base_usercount = 0
-            for single_date in Date(date).rangeto(Date(date).adddays(lost_day)):
-                date_string = ""
-                if Date(single_date).between(self.end_date) <= 0:
-                    date_string += ",,,,,"
-                else:
-                    # 留存率查询
+            for k in all_lost_day:
+                # 留存率查询
+                day = k - 1
+                single_date = Date(date).adddays(day)
+                if Date(single_date).between(self.end_date) > 0:
                     current_lost_usercount = self.get_lost_count(
                         date, single_date)
+
+                    # 留存率
+                    # line_string += "{0:.2f}%,".format(100*float(
+                    #     firstopen_usercount - current_lost_usercount)/float(firstopen_usercount))
+
                     # 流失分布查询
                     lost_day_results = self.get_result(
-                        "plant_progress_of_lost_users.sql", date, single_date)
-                    if currentDayIndex == 1:
-                        progress_data_map = {}
-                        for k in range(0, chapter_id):
-                            lost_base_data = [k + 1, 0, 0]
-                            lost_base_datas.append(lost_base_data)
-                            progress_data_map[k + 1] = lost_base_data
-                        for row in lost_day_results:
-                            progress_data = progress_data_map[row.chapter_id]
-                            progress_data[1] = row.user_count
-                            progress_data[2] = 100 * \
-                                float(row.user_count) / \
+                        "流失用户关卡推进.sql", date, single_date)
+
+                    lost_base_datas = []
+                    for row in lost_day_results:
+                        lost_base_data = [row.max_stage, row.user_count,
+                                          100*float(row.user_count)/float(firstopen_usercount)]
+                        lost_base_datas.append(lost_base_data)
+                    first_lost_usercount = current_lost_usercount - \
+                        sum(t[1] for t in lost_base_datas)
+                    first_stage_found = False
+                    for item in lost_base_datas:
+                        if item[0] == 0:
+                            first_stage_found = True
+                            item[1] = item[1] + first_lost_usercount
+                            item[2] = 100*float(item[2]) / \
                                 float(firstopen_usercount)
-                        first_progress_data = progress_data_map[1]
-                        first_progress_data[1] = first_progress_data[1] + \
-                            current_lost_usercount - \
-                            sum(t[1] for t in lost_base_datas)
-                        first_progress_data[2] = 100 * \
-                            float(first_progress_data[1]) / \
-                            float(firstopen_usercount)
-                        lost_base_usercount = current_lost_usercount
-                        date_string += "{0:.2f}%,".format(100*float(
-                            firstopen_usercount - current_lost_usercount)/float(firstopen_usercount))
+                            break
+                    if not first_stage_found:
+                        lost_base_datas.insert(
+                            0, [0, first_lost_usercount, 100*float(first_lost_usercount)/float(firstopen_usercount)])
+
+                    line_index = min(day, 6)
+                    startline = 1 + 3 * line_index
+                    endline = 2 + 3 * line_index
+                    head_line = [x.strip()
+                                 for x in lines[startline:endline]][0]
+                    head_lines = head_line.split(',')
+                    for head in head_lines:
+                        headsegments = head.split('|')
+                        min_level = int(headsegments[0])
+                        max_level = sys.maxsize if len(
+                            headsegments) == 1 else int(headsegments[1])
+                        level_user_percent = 0
                         for k in range(len(lost_base_datas)):
                             data = lost_base_datas[k]
-                            date_string += "{0:.2f}%,".format(data[2])
-                    else:
-                        current_lost_datas = []
-                        progress_data_map = {}
-                        for k in range(0, chapter_id):
-                            current_lost_data = [k + 1, 0, 0]
-                            current_lost_datas.append(current_lost_data)
-                            progress_data_map[k + 1] = current_lost_data
-                        for row in lost_day_results:
-                            progress_data = progress_data_map[row.chapter_id]
-                            progress_data[1] = row.user_count
-                            progress_data[2] = 100 * \
-                                float(row.user_count) / \
-                                float(firstopen_usercount)
-                        first_progress_data = progress_data_map[1]
-                        first_progress_data[1] = first_progress_data[1] + \
-                            current_lost_usercount - \
-                            sum(t[1] for t in current_lost_datas)
-                        first_progress_data[2] = 100 * \
-                            float(first_progress_data[1]) / \
-                            float(firstopen_usercount)
-                        lost_base_usercount = current_lost_usercount
-                        date_string += "{0:.2f}%,".format(100*float(
-                            firstopen_usercount - current_lost_usercount)/float(firstopen_usercount))
-                        for k in range(len(current_lost_datas)):
-                            data = current_lost_datas[k]
-                            base_data = lost_base_datas[k]
-                            date_string += "{0:.2f}%,".format(
-                                data[2] - base_data[2])
-                        lost_base_datas = current_lost_datas
+                            if data[0] >= min_level and data[0] <= max_level:
+                                level_user_percent += data[2]
+                        line_string += "{0:.2f}%,".format(level_user_percent)
 
-                diffDay = Date(date).between(single_date)
-                if diffDay <= 15 or diffDay in extra_retation_date:
-                    line_string += date_string
-                # 增加天数索引
-                currentDayIndex += 1
             # 数据拼接
             append_line(report_lines, len(report_lines), line_string)
             file.close()
